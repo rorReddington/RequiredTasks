175e5be260d227dce0384e5627acaea018eb9259
rorian <rorian.export@gmail.com>
2018-12-24 20:40:41 +0300

    Fixed 12 task

--- Task 12/main.c
+++ Task 12/main.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
-char spaces_delete(char *buffer) {
-    
+char *spaces_delete(char *buff) {
+
+    char *buffer = buff;
     size_t size = strlen(buffer);
-    
+
     for(size_t i = 0; i < size; i++) {
-        
+
         if(buffer[i] == ' ' && buffer[i+1] == ' ') {
-            
+
             for(size_t j = i; j < size; j++) {
-                
+
                 buffer[j] = buffer[j+1];
             }
             --i;
         }
     }
-    
+
     if (buffer[0] == ' ') {
-        
-        for(size_t i = 0; i < size; i++) {
-            
+
+        for(size_t i = 0; i < size; i++)
             buffer[i] = buffer[i+1];
-        }
     }
+
+    size = strlen(buffer);
+
+    if (buffer[size-1] == ' ')
+        buffer[size-1] = '\0';
+
+    return buffer;
 }
 
 int main(int argc, char* argv[])
 {
     system("clear");
-    
+
     char buffer[256];
-    
+
     gets(buffer);
-    
-    spaces_delete(buffer);
-    
-    puts(buffer);
-    
+
+    char* newBuffer = spaces_delete(buffer);
+
+    puts(newBuffer);
+
+    printf("strlen = %d\n", strlen(newBuffer));
+
     return 0;
 }
